184890ad83c6e8d1a68a76177baf23b76352ebe4
Walter Heck <walter@tribily.com>
2012-06-10 03:42:25 +0800

    Some more updates to cooltool

--- src/cooltool.rb
+++ src/cooltool.rb
@@ -4,39 +4,58 @@ require 'rubygems'
 require 'net/ssh'
 # require 'vagrant'
 
-print "CoolTool\n"
+puts "CoolTool\n"
 
 legacyIp        = "192.168.56.201"
 legacyUsername  = "root"
-legacyPort      = 37923
 legacyPassword  = "root"
-# legacyKey = "/Users/olindata/.ssh/od-linode"
+legacyPort      = 37923
 
 findHostNameCmd = "hostname"
-# createBlueprintCmd = "blueprint create"
+createBlueprintCmd = "blueprint create"
 
-print "Connecting to Legacy box " + legacyIp + " using " + legacyUsername + "/" + legacyPassword + "\n"
+puts "Connecting to Legacy box " + legacyIp + " using " + legacyUsername + "/" + legacyPassword 
 
 # Connect to the legcy system
 Net::SSH.start( legacyIp, legacyUsername, 
                 :password => legacyPassword,
                 :port     => legacyPort
-                ) do |ssh|
-  print "Connected!\n"
+                ) do |session|
+  # Show we're connected
+  puts "....Connected!\n"
+  
+  session.open_channel do |channel|
 
-  legacyHostname = (ssh.exec! findHostNameCmd).delete("\n")
-  currentTimestamp = Time.now.strftime("%d%m%Y%H%M%S")
-  #puts ssh.exec! findHostNameCmd
-  blueprintName = legacyHostname + "-" + currentTimestamp
-  # puts "" + legacyHostname + "-" + currentTimestamp
-  
-  blueprintCmd = createBlueprintCmd + " " + blueprintName
-  # blueprintCmd = "uname -a"
-  print blueprintCmd + "\n"
-  
-  print "Create blueprint repo" + "\n"
-  # puts ssh.exec! blueprintCmd 
-  
+    legacyHostname = (session.exec! findHostNameCmd).delete("\n")
+    currentTimestamp = Time.now.strftime("%d%m%Y%H%M%S")
+    puts session.exec! findHostNameCmd
+    blueprintName = legacyHostname + "-" + currentTimestamp
+    # puts "" + legacyHostname + "-" + currentTimestamp
+    
+    blueprintCmd = createBlueprintCmd + " " + blueprintName
+    print blueprintCmd + "\n"
+
+    channel.exec(blueprintCmd) do |ch, success|
+      abort "could not execute command" unless success
+
+      channel.on_data do |ch, data|
+        puts "got stdout: #{data}"
+        channel.send_data "something for stdin\n"
+      end
+
+      channel.on_extended_data do |ch, type, data|
+        puts "got stderr: #{data}"
+      end
+
+      channel.on_close do |ch|
+        puts "channel is closing!"
+      end
+    end  
+
+    # print "Create blueprint repo" + "\n"
+
+  end
+
 end
 
 # # bring up the vagrant box
